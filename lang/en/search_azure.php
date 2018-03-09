c5d7c20d4a81ec38011e2722f3fe1f1804775d06
Matt Porritt <mattp@catalyst-au.net>
2018-03-09 13:35:30 +1100

    fix capitalisation of lang string to be consistent with project standard. Fixes #2

--- lang/en/search_azure.php
+++ lang/en/search_azure.php
@@ -30,13 +30,13 @@ $string['pluginname_help'] = 'Search backend for the Azure Search search engine'
 
 $string['addfail'] = 'Failed to add document to index';
 $string['adminsettings'] = 'Plugin settings';
-$string['apikey'] = 'API Key';
+$string['apikey'] = 'API key';
 $string['apikey_help'] = 'An Azure Search Admin API related to the service you want to use.';
 $string['apiversion'] = 'API version';
 $string['apiversion_help'] = 'The Azure Search Admin API version to use, (normally the this should alwasy be the default).';
 $string['azuresettings'] = 'Azure Search settings';
 $string['deletefail'] = 'Failed to delete document from index';
-$string['enrichsettings'] = 'Data Enrichment settings';
+$string['enrichsettings'] = 'Data enrichment settings';
 $string['fileindexing'] = 'Enable file indexing';
 $string['fileindexing_help'] = 'Enables file indexing for this plugin. With this option checked you will need to enter details of the Tika service in the "File indexing settings" below.<br/>
 You will need to reindex all site contents after enabling this option for all files to be added.';
@@ -46,16 +46,16 @@ $string['index'] = 'Index';
 $string['index_help'] = 'Namespace index to store search data in backend';
 $string['noconfig'] = 'Azure Search configuration missing';
 $string['noserver'] = 'Azure Search endpoint unreachable';
-$string['pluginsettings'] = 'Plugin Settings';
+$string['pluginsettings'] = 'Plugin settings';
 $string['searchinfo'] = 'Search queries';
 $string['searchinfo_help'] = 'The field to be searched may be specified by prefixing the search query with \'title:\', \'content:\', \'name:\', or \'intro:\'. For example, searching for \'title:news\' would return results with the word \'news\' in the title.
 
 Boolean operators (\'AND\', \'OR\') may be used to combine or exclude keywords.';
 $string['searchurl'] = 'URL';
 $string['searchurl_help'] = 'The FQDN of the Azure Search engine endpoint';
-$string['tikahostname'] = 'Tika Hostname';
+$string['tikahostname'] = 'Tika hostname';
 $string['tikahostname_help'] = 'The FQDN of the Apache Tika endpoint';
-$string['tikaport'] = 'Tika Port';
-$string['tikaport_help'] = 'The Port of the Apache Tika endpoint';
+$string['tikaport'] = 'Tika port';
+$string['tikaport_help'] = 'The port of the Apache Tika endpoint';
 $string['tikasendsize'] = 'Maximum file size';
 $string['tikasendsize_help'] = 'Sending large files to Tika can cause out of memory issues. Therefore we limit it to a size in bytes.';
